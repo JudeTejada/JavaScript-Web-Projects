4fdbf237b384e5e0db35bf97b4b9ca583ea362fa
JudeTejada <judelaurentetejada@gmail.com>
2020-04-15 03:44:36 -0500

    Neumorphism Clock

---
+++ digitalClock/src/app.js
@@ -0,0 +1,18 @@
+function setTime() {
+  const time = new Date();
+  let minute = time.getMinutes();
+  let hours = time.getHours();
+
+  const ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minute = minute < 10 ? "0" + minute : minute;
+
+  const digitalTime = `${hours} : ${minute} : ${ampm}`;
+
+  document.querySelector(".clock").innerHTML = `
+ <h1>${hours} : ${minute} <span>${ampm}</span></h1>
+ `;
+}
+
+setInterval(setTime, 1000);
